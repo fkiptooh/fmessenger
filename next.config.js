5a06d9bbbb15f05b9facbf33f0e97e647dbd9a84
fkiptooh <fkiptooh@gmail.com>
2023-05-12 20:30:18 +0300

    People's page

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
         domains: [
           "avatars.githubusercontent.com",
           "lh3.googleusercontent.com",
+          "lh3.googleusercontent.com",
           "res.cloudinary.com",
           "web.facebook.com"
         ]
